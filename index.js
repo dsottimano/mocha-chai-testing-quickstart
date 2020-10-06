25a602548e71af252a1b1dd17b7fb8d376046b46
dsottimano <dsottimano@gmail.com>
2020-10-06 17:00:55 -0500

    async added

--- index.js
+++ index.js
@@ -1,28 +1,82 @@
 'use strict';
 
 const chai = require('chai')
-  , expect = chai.expect
-  , should = chai.should();
+    , expect = chai.expect
+    , should = chai.should();
 
 
 function iseven(num) {
     return num % 2 === 0;
 }
 
-describe('iseven',()=>{
-    var num;
+function getTime(ms) {
+    return new Promise(resolve => {
+        setTimeout(function () {
 
-    beforeEach(()=>{
-        num= 1
+            resolve("helloo")
+
+        }, 3000);
+    })
+}
+
+
+//async function, promise returned after 3 seconds
+describe("async test", function () {
+
+    //must pass in done, also, catch done to report error
+    //no es6!
+    //adjust timeout to 10 seconds or mocha will complain after 2s
+    it('async timer event done', function (done) {
+        this.timeout(10000)
+        getTime().then(function(data) {
+            data.should.equal("hello")
+            done();
+
+
+        }).catch(done)
     })
 
-    afterEach(()=>{
-       num = 1
-    })
-
-
-    
-    it('should return true when number is even',() =>{
-        iseven(num).should.be.true
-    }) 
 })
+
+
+describe('number tests', () => {
+
+
+
+
+    describe('iseven', () => {
+        var num;
+
+        beforeEach(() => {
+            num = 2
+        })
+
+        afterEach(() => {
+            //console.log("done")
+        })
+
+
+
+        it('should return true when number is even', () => {
+            iseven(num).should.be.true
+        })
+
+
+
+
+        //skipping tests
+        //we can skip a test by using .skip
+        it.skip('skip test example 1', () => {
+            iseven(num).should.be.true
+        })
+
+    })
+
+    //we can also skip describing by adding skip
+    describe.skip('skip describe', () => {
+
+    })
+
+})
+
+
